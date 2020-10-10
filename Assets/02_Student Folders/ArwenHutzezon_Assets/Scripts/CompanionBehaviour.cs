0ec6034e6e55a06d75956e8130ff796f82dd87d3
ArwenTS <Arwen.Tiger@gmail.com>
2020-10-10 21:54:31 +0200

    W.I.P.

---
+++ Assets/02_Student Folders/ArwenHutzezon_Assets/Scripts/CompanionBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _rightDistanceFromPlayer = 2;
+    [SerializeField] private float _forwardDistanceFromPlayer = 2;
+
+    private Vector3 _targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _targetPosition = _player.transform.position + _player.transform.right * _rightDistanceFromPlayer + _player.transform.forward * _forwardDistanceFromPlayer + _player.transform.up * 0.75f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _targetPosition = _player.transform.position + _player.transform.right * _rightDistanceFromPlayer + _player.transform.forward * _forwardDistanceFromPlayer + _player.transform.up * 0.75f;
+        Debug.Log(_targetPosition);
+        transform.position = _targetPosition;
+    }
+}
