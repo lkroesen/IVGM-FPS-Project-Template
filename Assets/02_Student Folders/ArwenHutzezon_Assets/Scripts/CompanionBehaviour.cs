b0f0f4c87072015150f22aea0909488fe64b5bc7
Switch-48 <max.van.steenis@gmail.com>
2020-10-14 16:23:29 +0200

    Added a behaviour component for the Companion

--- Assets/02_Student Folders/ArwenHutzezon_Assets/Scripts/CompanionBehaviour.cs
+++ Assets/02_Student Folders/ArwenHutzezon_Assets/Scripts/CompanionBehaviour.cs
@@ -1,25 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CompanionBehaviour : MonoBehaviour
 {
-    [SerializeField] private GameObject _player;
-    [SerializeField] private float _rightDistanceFromPlayer = 2;
-    [SerializeField] private float _forwardDistanceFromPlayer = 2;
+    [Header("Follower stats")]
+    [SerializeField] private float _speed = 10;
+    [SerializeField] private float _rotateSpeed = 5;
+    [SerializeField] private float _gravityDownForce = 10;
+    
+    [Header("Follow positioning")]
+    [SerializeField] private GameObject _packLeader;
 
-    private Vector3 _targetPosition;
+    [SerializeField] private float _yOffset = 1;
+    [SerializeField] private float _minFollowDistance = 3;
+
+    private Transform _target;
+    private PlayerCharacterController _playerCharacterController;
+    private Vector3 _lookDirection;
+    private Vector3 _targetDirection;
+
+    private Vector3 _currentTarget;
+
+    private Rigidbody _rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        _targetPosition = _player.transform.position + _player.transform.right * _rightDistanceFromPlayer + _player.transform.forward * _forwardDistanceFromPlayer + _player.transform.up * 0.75f;
+        _target = _packLeader.GetComponent<Transform>();
+        _playerCharacterController = _packLeader.GetComponent<PlayerCharacterController>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _targetPosition = _player.transform.position + _player.transform.right * _rightDistanceFromPlayer + _player.transform.forward * _forwardDistanceFromPlayer + _player.transform.up * 0.75f;
-        Debug.Log(_targetPosition);
-        transform.position = _targetPosition;
+        _currentTarget = _target.position;
+        Follow();
+    }
+
+    private void Follow()
+    {
+        if (Vector3.Distance(transform.position, _currentTarget) > _minFollowDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                _currentTarget + new Vector3(0,_yOffset,0),
+                _speed * Time.deltaTime);
+
+            _targetDirection = _currentTarget - transform.position;
+            _lookDirection = Vector3.RotateTowards(transform.forward, 
+                _targetDirection + new Vector3(0, _yOffset, 0),
+                _rotateSpeed * Time.deltaTime, 0.0f);
+            transform.rotation = Quaternion.LookRotation(_lookDirection);
+        }
     }
 }
