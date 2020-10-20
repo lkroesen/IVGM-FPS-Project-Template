8d9dea9297f3d9167ae263c8201a8b97be5a1209
Higaphix <36793967+Higaphix@users.noreply.github.com>
2020-10-20 21:56:25 +0200

    Yeet 3

---
+++ Assets/02_Student Folders/RobinSmit_Assets/Scripts/GravityPickup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GravityPickup : MonoBehaviour
+{
+    [Header("Parameters")]
+    [Tooltip("Newgravity after picking up")]
+    public float NewGravity;
+    public float jumppower;
+
+    Pickup m_Pickup;
+
+    void Start()
+    {
+        m_Pickup = GetComponent<Pickup>();
+        DebugUtility.HandleErrorIfNullGetComponent<Pickup, GravityPickup>(m_Pickup, this, gameObject);
+
+        // Subscribe to pickup action
+        m_Pickup.onPick += OnPicked;
+    }
+
+    void OnPicked(PlayerCharacterController player)
+    {
+        PlayerCharacterController playerPlayerCharacterController = player.GetComponent<PlayerCharacterController>();
+
+	playerPlayerCharacterController.SetGravity(NewGravity, jumppower);
+
+        m_Pickup.PlayPickupFeedback();
+
+        Destroy(gameObject);
+    }
+}
