d981a81cbff7ed1642e0a2a6c92555dc5e4bbe5b
LucaStoffels <32015566+LucaStoffels@users.noreply.github.com>
2020-10-22 00:00:39 +0200

    Final push

--- Assets/02_Student Folders/ArwenHutzezon_Assets/Scripts/CompanionBehaviour.cs
+++ Assets/02_Student Folders/ArwenHutzezon_Assets/Scripts/CompanionBehaviour.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.PackageManager;
+using System.Collections.Generic; //
 using UnityEngine;
 using UnityEngine.AI;
 
